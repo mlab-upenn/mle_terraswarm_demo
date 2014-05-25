eecbfac4e0aa51ff375f669b30b3ec5713d5b19c
Truong X. Nghiem <truong.nghiem@gmail.com>
2014-05-24 23:31:47 -0400

    Finished working spinUpEP.m. For examples/simple: updated run.m to test spinning up; updated variables.cfg for spinUpEP.

--- examples/simple/run.m
+++ examples/simple/run.m
@@ -1,6 +1,7 @@
 % (C) 2014 by Truong X. Nghiem (nghiem@seas.upenn.edu)
 %
 % CHANGES:
+%   2014-05-24  Added spinning up to a given time.
 %   2014-05-06  Truong started
 
 %% Create an mlepProcess instance and configure it
@@ -34,8 +35,29 @@ MAXSTEPS = 1*24*EPTimeStep;  % max simulation time (first number is in days)
 % logdata stores the electricity demand of the entire plant
 logdata = zeros(MAXSTEPS, 6);
 
+% Spin up E+ to a certain time
+disp('Spinning E+ up to given time...');
+try
+    [nextStep, flag, eptime, outputs] = spinUpEP(ep, 1/EPTimeStep, 1, 10, 5);
+catch err
+    ep.stop;
+    rethrow(err);
+end
+disp('Finished spinning up E+. Now do simulation...');
+
+% IMPORTANT NOTE: after spinning up, the message from E+ for the current
+% time step has already been read. We must write to E+ in order to advance
+% in time.
+
 while kStep <= MAXSTEPS
     disp('Tick');
+    
+    % Save to logdata
+    logdata(kStep, :) = outputs;
+    
+    % Write to inputs of E+
+    % Because we don't input anything to E+, the message is empty
+    ep.write(mlepEncodeRealData(VERNUMBER, 0, eptime, []));    
     
     % Read a data packet from E+
     packet = ep.read;
@@ -46,13 +68,6 @@ while kStep <= MAXSTEPS
     % Parse it to obtain building outputs
     [flag, eptime, outputs] = mlepDecodePacket(packet);
     if flag ~= 0, break; end
-        
-    % Write to inputs of E+
-    % Because we don't input anything to E+, the message is empty
-    ep.write(mlepEncodeRealData(VERNUMBER, 0, (kStep-1)*deltaT, []));    
-
-    % Save to logdata
-    logdata(kStep, :) = outputs;
     
     kStep = kStep + 1;
 end
@@ -70,9 +85,9 @@ end
 
 % Plot results
 fprintf('The current date is %d-%d-%d, which is day %d (1=Sun,...,7=Sat).\n',...
-    logdata(1, 3), logdata(1, 2), logdata(1, 1), logdata(1, 4));
-logdata(logdata(:,5) >= 24, 5) = 0;
-plot(logdata(:, 5), logdata(:, 6));
+    logdata(1, 3), logdata(1, 2), logdata(1, 1), logdata(1, 5));
+logdata(logdata(:,4) >= 24, 4) = 0;
+plot(logdata(1, 4) + (0:(size(logdata,1)-1))/EPTimeStep, logdata(:, 6)/1000);
 title('Electricity demand of entire plant');
 xlabel('Time (hour)');
-ylabel('Demand [W]');
+ylabel('Demand [kW]');
--- spinUpEP.m
+++ spinUpEP.m
@@ -1,9 +1,9 @@
-function nextStep = spinUpEP(EP, TS, M, D, TOD, FB)
+function [nextStep, flag, simtime, outputs] = spinUpEP(EP, TS, M, D, TOD, FB)
 %SPINUPEP Spin up E+ simulation till a given time of year.
-%   nextStep = spinUpEP(EP, TS, M, D, TOD, FB)
+%   [nextStep, flag, simtime, outputs] = spinUpEP(EP, TS, M, D, TOD, FB)
 %spins up an EnergyPlus co-simulation EP (mlepProcess), with timestep TS
-%(in minutes), to a given date of year M/D (M: 1-12, D: 1-31) and time of
-%day TOD (0.0 to 24.0 as fraction of hours).  A requirement of the
+%(in hours, 0-1), to a given date of year M/D (M: 1-12, D: 1-31) and time
+%of day TOD (0.0 to 24.0 as fraction of hours).  A requirement of the
 %co-simulation is that it must export the following variables at the exact
 %indices:
 % 1. currentYear
@@ -25,7 +25,17 @@ function nextStep = spinUpEP(EP, TS, M, D, TOD, FB)
 %
 %The output nextStep is a structure of fields (M, D, TOD) that specifies
 %the next time step of the co-simulation.  It can be used by the caller to
-%ensure time synchronization with other processes.
+%ensure time synchronization with other processes.  If spinning up is
+%successful, nextStep is strictly later than (M, D, TOD) while the current
+%time step of EnergyPlus is no later than (M, D, TOD), i.e.
+%       current-time-step <= (M, D, TOD) < nextStep
+%
+%After spinning up, the message from E+ for the current time step has
+%already been read.  Its results are given in the outputs: flag is the flag
+%in the message, simtime is the simulation time in E+, outputs is the
+%vector of outputs from E+ (including the time values above).  So the
+%caller code should start writing to E+ instead of reading from E+ (which
+%will result in time-out error).
 %
 %The function will stop and give an error if the given time (M, D, TOD) is
 %before the current time in EnergyPlus.  To be safe, make sure that the E+
@@ -33,6 +43,143 @@ function nextStep = spinUpEP(EP, TS, M, D, TOD, FB)
 %
 %(C) 2014 by Truong X. Nghiem (nghiem@seas.upenn.edu)
 
+% Check input arguments
+narginchk(5, 6);
+
+assert(isa(EP, 'mlepProcess'), 'EP must be an object of class mlepProcess.');
+assert(EP.isRunning, 'EP-cosim object must be already running (use method start).');
+
+assert(0 < TS && TS <= 1, 'Timestep TS must be between (0, 1].');
+
+assert(rem(M, 1) == 0 && 1 <= M && M <= 12, 'Month M must be a whole number between 1 and 12.');
+assert(rem(D, 1) == 0 && 1 <= D && D <= maxDays(M, true), 'Day of month D must be a whole number and valid for the given month.');
+assert(0 <= TOD && TOD < 24, 'Time of day TOD must be between 0 and 24.');
+
+hasFB = nargin > 5 && ~isempty(FB);
+if hasFB
+    assert(isa(FB, 'function_handle'), 'Feedback function FB must be a function handle.');
+end
+
+
+%% Main algorithm: Spin up E+
+
+% Initialize these variables here to be updated by nested function(s)
+EPY = 0; EPM = 0; EPD = 0; EPTOD = 0;
+
+% First read from E+ to determine current time and year
+[flag, simtime, outputs] = readFromEP;
+if flag ~= 0
+    % E+ quits
+    error('spinUpEP:EPQuit', 'EnergyPlus terminates prematurely with flag = %d.', flag);
+end
+
+% If EP's year is not a leap year but M = 2, D = 29 then adjust D because
+% Feb 29th does not exist.
+leapyear = isLeapYear(EPY);
+if M == 2 && D == 29 && ~leapyear
+    warning('spinUpEP:noFeb29', 'There is no Feb 29th for the year in EnergyPlus. Adjusted to Feb 28th instead.');
+    D = 28;
+end
+
+if isLaterTime(EPM, EPD, EPTOD, M, D, TOD)
+    error('spinUpEP:InvalidTime', 'EnergyPlus'' current time is later than the desired time.');
+end
+
+inputs = [];  % the inputs to E+ (default to [] if no feedback)
+
+nextStep = struct('M', 0, 'D', 0, 'TOD', 0);
+[nextStep.M, nextStep.D, nextStep.TOD] = addTime(EPM, EPD, EPTOD, TS, leapyear);
+
+while ~isLaterTime(nextStep.M, nextStep.D, nextStep.TOD, M, D, TOD)
+    if hasFB
+        inputs = FB(outputs);
+    end
+    
+    % Write to inputs of E+
+    EP.write(mlepEncodeRealData(EP.version, 0, simtime, inputs));
+    
+    % Read from E+
+    [flag, simtime, outputs] = readFromEP;
+    if flag ~= 0
+        % E+ quits
+        error('spinUpEP:EPQuit', 'EnergyPlus terminates prematurely with flag = %d.', flag);
+    end
+
+    [nextStep.M, nextStep.D, nextStep.TOD] = addTime(EPM, EPD, EPTOD, TS, leapyear);
+end
+
+% At this point, we should have nextStep exceeding desired time. We can
+% return now.
+
+    % Helper functions
+    function [flag, simtime, outputs] = readFromEP
+        packet = EP.read;
+        if isempty(packet)
+            error('spinUpEP:commError', 'Could not read outputs from E+.');
+        end
+        
+        % Parse it to obtain building outputs
+        [flag, simtime, outputs] = mlepDecodePacket(packet);
+        EPY = outputs(1);
+        EPM = outputs(2);
+        EPD = outputs(3);
+        EPTOD = outputs(4);
+    end
 
 end
 
+function [m, d, tod, nextyear] = addTime(m, d, tod, ts, leapyear)
+% Add ts (fraction of hours) to the current time (d, m, tod).
+% leapyear is true if the current year is a leap year.
+% ts should be small (not more than 24 (hours))
+% nextyear = true if the time rolls to next year (however because we don't
+% consider the year, (m,d) goes back to beginning of year.
+% No error checking is carried out
+
+nextyear = false;
+tod = tod + ts;
+if tod >= 24
+    tod = tod - 24;
+    d = d + 1;
+    maxd = maxDays(m, leapyear);
+    if d > maxd
+        d = d - maxd;
+        m = m + 1;
+        if m > 12
+            m = m - 12;
+            nextyear = true;
+        end
+    end
+end
+
+end
+
+function md = maxDays(m, leapyear)
+% return max number of days for a given month
+% No error checking is carried out
+switch m
+    case {1, 3, 5, 7, 8, 10, 12}
+        md = 31;
+    case 2
+        if leapyear
+            md = 29;
+        else
+            md = 28;
+        end
+    otherwise
+        md = 30;
+end
+end
+
+function ly = isLeapYear(y)
+ly = (rem(y, 4) == 0) && (rem(y, 100) ~= 0 || rem(y, 400) == 0);
+end
+
+function l = isLaterTime(m1, d1, tod1, m2, d2, tod2)
+% Check if time1 > time2 (strictly)
+% Convert (m,d,tod) to a number then compare
+% the coefficients
+wd = 32; % wd >= 24
+wm = 32*wd;  % >= (31+1)*wd >= 31*wd + 24
+l = (m1*wm + d1*wd + tod1) > (m2*wm + d2*wd + tod2);
+end
